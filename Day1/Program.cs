7f6a1e1ab7af45a8e99b0be52d1e9461d1e6c6ac
Markus Bohnert <markusbohnert@Markuss-MBP.fritz.box>
2023-12-08 20:07:16 +0100

    Initial commit Add first day task

---
+++ Day1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File.ReadAllLines("Input.txt");
+var numbers = new List<int>(lines.Length);
+char[] digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+foreach (var line in lines) {
+	var firstDigit = line[line.IndexOfAny(digits)];
+	var lastDigit = line[line.LastIndexOfAny(digits)];
+	numbers.Add(int.Parse($"{firstDigit}{lastDigit}"));
+}
+
+Console.WriteLine(string.Join(" + ", numbers));
+Console.WriteLine($"The sum is: {numbers.Sum()}");
