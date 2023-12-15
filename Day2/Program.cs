d12efba1cac875c3d653d7c9a14e286b220ff53d
Markus Bohnert <markusbohnert@Markuss-MBP.fritz.box>
2023-12-15 17:41:18 +0100

    Implement part 2 of Day 2

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -1,23 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 var lines = File.ReadAllLines("Input.txt");
 var possibleGameIds = new List<int>();
+var gameCubePowers = new List<int>();
 
 const int maxAmountOfRedCubes = 12;
 const int maxAmountOfBlueCubes = 14;
 const int maxAmountOfGreenCubes = 13;
 
 foreach (var line in lines) {
-	var gameId = ValidateGame(line);
+	var gameId = ValidateGamesForMaxCubes(line);
+	gameCubePowers.Add(CalculatePowerOfCubes(line));
 	if (!string.IsNullOrEmpty(gameId)) {
 		possibleGameIds.Add(int.Parse(gameId));
 	}
 }
 
-static string ValidateGame(string line) {
-	var gameId = line[..line.IndexOf(':')].Remove(0, 5);
-	var gameOutput = line[line.IndexOf(':')..].Remove(0,2);
+Console.WriteLine("Part 1");
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine($"Possible game Ids: {string.Join(',', possibleGameIds)}");
+Console.WriteLine($"Sum of possible game Ids: {possibleGameIds.Sum()}");
 
-	var cubeSets = gameOutput.Split("; ");
+Console.WriteLine();
+
+Console.WriteLine("Part 2");
+Console.WriteLine("--------------------------------------------------");
+Console.WriteLine($"Cube powers: {string.Join(',', gameCubePowers)}");
+Console.WriteLine($"Sum of cube powers: {gameCubePowers.Sum()}");
+
+return;
+
+static string GetGameId(string gameInput) {
+	return gameInput[..gameInput.IndexOf(':')].Remove(0, 5);
+}
+
+static string GetGameOutput(string gameInput) {
+	return gameInput[gameInput.IndexOf(':')..].Remove(0,2);
+}
+
+static string[] GetCubeSets(string gameOutput) {
+	return gameOutput.Split("; ");
+}
+
+// Part 1
+static string ValidateGamesForMaxCubes(string gameInput) {
+	var gameId = GetGameId(gameInput);
+	var gameOutput = GetGameOutput(gameInput);
+	var cubeSets = GetCubeSets(gameOutput);
 	
 	foreach (var cubeSet in cubeSets) {
 		var cubes = cubeSet.Split(", ");
@@ -48,5 +76,44 @@ static string ValidateGame(string line) {
 	return gameId;
 }
 
-Console.WriteLine($"Possible game Ids: {string.Join(',', possibleGameIds)}");
-Console.WriteLine($"Sum of possible game Ids: {possibleGameIds.Sum()}");
+// Part 2
+// Find the minimum set of cubes that can be used to build a game
+// Return the multiplication of the cubes
+static int CalculatePowerOfCubes(string gameInput) {
+	var powerOfCubes = 0;
+	var minAmountOfRedCubes = 0;
+	var minAmountOfBlueCubes = 0;
+	var minAmountOfGreenCubes = 0;
+	var gameOutput = GetGameOutput(gameInput);
+	var cubeSets = GetCubeSets(gameOutput);
+	
+	foreach (var cubeSet in cubeSets) {
+		var cubes = cubeSet.Split(", ");
+		foreach (var cube in cubes) {
+			var cubeAmount = int.Parse(cube[..cube.IndexOf(' ')]);
+			var cubeColor = cube[cube.IndexOf(' ')..].Remove(0,1);
+
+			switch (cubeColor) {
+				case "red":
+					if (cubeAmount > minAmountOfRedCubes) {
+						minAmountOfRedCubes = cubeAmount;
+					}
+					break;
+				case "green":
+					if (cubeAmount > minAmountOfGreenCubes) {
+						minAmountOfGreenCubes = cubeAmount;
+					}
+					break;
+				case "blue":
+					if (cubeAmount > minAmountOfBlueCubes) {
+						minAmountOfBlueCubes = cubeAmount;
+					}
+					break;
+			}
+		}
+	}
+	
+	powerOfCubes = minAmountOfRedCubes * minAmountOfBlueCubes * minAmountOfGreenCubes;
+	
+	return powerOfCubes;
+}
