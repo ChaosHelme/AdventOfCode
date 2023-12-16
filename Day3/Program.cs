a4dd76fc12bb772ddc0131321ae8ba767fabdc97
Markus Bohnert <bohnert.markus@googlemail.com>
2023-12-16 20:01:48 +0100

    Partial implement part 2 of Day 3
    Gear ratio left and right can be found

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -19,6 +19,7 @@ var amountOfColumns = lines[0].Length;
 
 Console.WriteLine($"{amountOfRows} rows and {amountOfColumns} columns");
 
+// Part 1
 for (var row = 0; row < amountOfRows; row++) {
 	var startIndex = 0;
 	while (startIndex < amountOfColumns) {
@@ -45,11 +46,86 @@ for (var row = 0; row < amountOfRows; row++) {
 	}
 }
 
-foreach (var partNumber in partNumbers) {
-	Console.WriteLine(partNumber.ToString());
+Console.WriteLine($"The sum of all part numbers is {partNumbers.Sum(p => p.Value)}");
+
+var gearRatios = new List<int>();
+
+// Part 2
+for (var row = 0; row < amountOfRows; row++) {
+	var startIndex = 0;
+	while (startIndex < amountOfColumns) {
+		var indexOfSymbol = lines[row].IndexOf('*', startIndex);
+		if (indexOfSymbol < 0) {
+			break;
+			
+		}
+		startIndex = indexOfSymbol + 1;
+
+		var firstNumber = 0;
+		var secondNumber = 0;
+		
+		// Find number left of symbol
+		var numberLeftOfSymbol = lines[row][indexOfSymbol - 1];
+		if (digits.Contains(numberLeftOfSymbol)) {
+			var numberLeftOfSymbolIndex = indexOfSymbol - 1;
+			while (numberLeftOfSymbolIndex > 0 && digits.Contains(lines[row][numberLeftOfSymbolIndex - 1])) {
+				numberLeftOfSymbolIndex--;
+			}
+			
+			firstNumber = int.Parse(lines[row][numberLeftOfSymbolIndex..indexOfSymbol]);
+		}
+		
+		// Find number right of symbol
+		var numberRightOfSymbol = lines[row][indexOfSymbol + 1];
+		if (digits.Contains(numberRightOfSymbol)) {
+			var numberRightOfSymbolIndex = indexOfSymbol + 1;
+			while (numberRightOfSymbolIndex < amountOfColumns && digits.Contains(lines[row][numberRightOfSymbolIndex + 1])) {
+				numberRightOfSymbolIndex++;
+			}
+			
+			secondNumber = int.Parse(lines[row][(indexOfSymbol + 1)..(numberRightOfSymbolIndex + 1)]);
+		}
+
+		if (firstNumber > 0 && secondNumber > 0) {
+			gearRatios.Add(firstNumber * secondNumber);
+			continue;
+		}
+		
+		// Find number above symbol
+		var numberAboveSymbol = lines[row > 0 ? row - 1 : 0][indexOfSymbol];
+		if (digits.Contains(numberAboveSymbol)) {
+			var indexLeftOfSymbol = indexOfSymbol - 1;
+			var indexRightOfSymbol = indexOfSymbol + 1;
+			while (row > 0 && digits.Contains(lines[row - 1][indexLeftOfSymbol - 1])) {
+				indexLeftOfSymbol--;
+			}
+
+			while (row > 0 && digits.Contains(lines[row - 1][indexRightOfSymbol + 1])) {
+				indexRightOfSymbol++;
+			}
+			
+			firstNumber = int.Parse(lines[row - 1][indexLeftOfSymbol..indexRightOfSymbol]);
+		}
+		
+		// Find number below symbol
+		var numberBelowSymbol = lines[row < amountOfRows - 1 ? row + 1 : amountOfRows - 1][indexOfSymbol];
+		if (digits.Contains(numberBelowSymbol)) {
+			var numberBelowSymbolIndex = row < amountOfRows - 1 ? row + 1 : amountOfRows - 1;
+			while (numberBelowSymbolIndex < amountOfRows - 1 && digits.Contains(lines[numberBelowSymbolIndex + 1][indexOfSymbol])) {
+				numberBelowSymbolIndex++;
+			}
+			
+			secondNumber = int.Parse(lines[numberBelowSymbolIndex][indexOfSymbol].ToString());
+		}
+
+		if (firstNumber > 0 && secondNumber > 0) {
+			gearRatios.Add(firstNumber * secondNumber);
+			continue;
+		}
+	}
 }
 
-Console.WriteLine($"The sum of all part numbers is {partNumbers.Sum(p => p.Value)}");
+Console.WriteLine($"The sum of all gear ratios is {gearRatios.Sum()}");
 
 return;
 
