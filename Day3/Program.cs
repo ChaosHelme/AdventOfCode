396e1d4f481c180e4065f04dd94f59cf4b702843
Markus Bohnert <bohnert.markus@googlemail.com>
2023-12-16 14:48:47 +0100

    Implement Part 1 of Day 3
    Numbers can be found left, rigt, above, belof and diagonal of a symbol

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -6,6 +6,12 @@ char[] digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 char[] symbols = {'#', '@', '/', '\\', '!', '$', '%', '&', '*', '?', '+', '-', '^', '~', '<', '>', '='};
 char[] symbolsIncludingPoint = {'#', '@', '/', '\\', '!', '$', '%', '&', '*', '?', '+', '-', '^', '~', '<', '>', '=', '.'};
 var lines = File.ReadAllLines("Input.txt");
+
+if (!lines.Any()) {
+	Console.WriteLine("No input");
+	return;
+}
+
 var partNumbers = new List<PartNumber>();
 var amountOfRows = lines.Length;
 // We assume that all rows have the same amount of columns (which is the case for the input)
@@ -16,22 +22,26 @@ Console.WriteLine($"{amountOfRows} rows and {amountOfColumns} columns");
 for (var row = 0; row < amountOfRows; row++) {
 	var startIndex = 0;
 	while (startIndex < amountOfColumns) {
-		var indexOfDigit = lines[row].IndexOfAny(digits, startIndex);
-		if (indexOfDigit < 0) {
+		var startDigitIndex = lines[row].IndexOfAny(digits, startIndex);
+		if (startDigitIndex < 0) {
 			break;
 		}
-		
-		var endIndexOfDigit = lines[row].IndexOfAny(symbolsIncludingPoint, indexOfDigit);
-		if (endIndexOfDigit < 0) {
-			endIndexOfDigit = amountOfColumns;
-		}
-		
-		// Find the number which is ascendant of a symbol
-		if (symbols.Contains(lines[row][indexOfDigit > 0 ? indexOfDigit -1 : 0]) || symbols.Contains(lines[row][endIndexOfDigit < amountOfColumns ? endIndexOfDigit : amountOfColumns - 1])) {
-			partNumbers.Add(new PartNumber(row, indexOfDigit, int.Parse(lines[row][indexOfDigit..endIndexOfDigit])));
+
+		var endDigitIndex = lines[row].IndexOfAny(symbolsIncludingPoint, startDigitIndex);
+		if (endDigitIndex < 0) {
+			endDigitIndex = amountOfColumns;
 		}
 
-		startIndex = endIndexOfDigit + 1;
+		// Find the number which is ascendant of a symbol
+		if (IsNumberRightOfSymbol(row, startDigitIndex, endDigitIndex) ||
+			IsNumberLeftOfSymbol(row, startDigitIndex, endDigitIndex) ||
+			IsNumberAboveSymbol(row, startDigitIndex, endDigitIndex) ||
+			IsNumberBelowSymbol(row, startDigitIndex, endDigitIndex) ||
+			IsNumberDiagonalOfSymbol(row, startDigitIndex, endDigitIndex)) {
+			partNumbers.Add(new PartNumber(row, startDigitIndex, int.Parse(lines[row][startDigitIndex..endDigitIndex])));
+		}
+
+		startIndex = endDigitIndex + 1;
 	}
 }
 
@@ -39,7 +49,61 @@ foreach (var partNumber in partNumbers) {
 	Console.WriteLine(partNumber.ToString());
 }
 
-// var stringsEnumerable = lines.SelectMany(line => line.Split('.').Where(part => !string.IsNullOrEmpty(part)));
-// foreach (var s in stringsEnumerable) {
-// 	Console.WriteLine(s);
-// }
+Console.WriteLine($"The sum of all part numbers is {partNumbers.Sum(p => p.Value)}");
+
+return;
+
+// A number is ascendant of a symbol if it is directly above, below, left, right or diagonal of a symbol
+
+bool IsNumberRightOfSymbol(int row, int startIndexColumn, int endIndexColumn) {
+	for (var currentIndex = startIndexColumn; currentIndex <= endIndexColumn; currentIndex++) {
+		if (symbols.Contains(lines[row][currentIndex > 0 ? currentIndex - 1 : 0])) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool IsNumberLeftOfSymbol(int row, int startIndexColumn, int endIndexColumn) {
+	for (var currentIndex = startIndexColumn; currentIndex <= endIndexColumn; currentIndex++) {
+		if (symbols.Contains(lines[row][currentIndex < amountOfColumns ? currentIndex : amountOfColumns - 1])) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool IsNumberAboveSymbol(int row, int startIndexColumn, int endIndexColumn) {
+	for (var currentIndex = startIndexColumn; currentIndex <= endIndexColumn; currentIndex++) {
+		if (symbols.Contains(lines[row > 0 ? row - 1 : 0][currentIndex < amountOfColumns ? currentIndex : amountOfColumns - 1])) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool IsNumberBelowSymbol(int row, int startIndexColumn, int endIndexColumn) {
+	for (var currentIndex = startIndexColumn; currentIndex <= endIndexColumn; currentIndex++) {
+		if (symbols.Contains(lines[row < amountOfRows - 1 ? row + 1 : amountOfRows - 1][currentIndex < amountOfColumns ? currentIndex : amountOfColumns - 1])) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool IsNumberDiagonalOfSymbol(int row, int startIndexColumn, int endIndexColumn) {
+	for (var currentIndex = startIndexColumn; currentIndex <= endIndexColumn; currentIndex++) {
+		if (symbols.Contains(lines[row > 0 ? row - 1 : 0][currentIndex > 0 ? currentIndex - 1 : 0]) ||
+			symbols.Contains(lines[row > 0 ? row - 1 : 0][currentIndex < amountOfColumns ? currentIndex : amountOfColumns - 1]) ||
+			symbols.Contains(lines[row < amountOfRows - 1 ? row + 1 : amountOfRows - 1][currentIndex > 0 ? currentIndex - 1 : 0]) ||
+			symbols.Contains(lines[row < amountOfRows - 1 ? row + 1 : amountOfRows - 1][currentIndex < amountOfColumns ? currentIndex : amountOfColumns - 1])) {
+			return true;
+		}
+	}
+
+	return false;
+}
