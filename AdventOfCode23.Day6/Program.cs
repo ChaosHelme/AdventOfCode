446f2e7bd669da2ac991c306a5e08a467d99f3ee
Markus Bohnert <markusbohnert@Markuss-MBP.fritz.box>
2023-12-25 11:08:26 +0100

    Add Day 6

---
+++ AdventOfCode23.Day6/Program.cs
@@ -0,0 +1,24 @@
+﻿using AdventOfCode23.Shared;
+
+var lines = FileHelper.ValidateAndReadInputFile("Input.txt");
+if (lines == null) {
+	return -1;
+}
+
+var races = new List<Race>();
+
+// Assuming that the first line is for time/duration
+var times = lines[0].Split(["Time:", " "], StringSplitOptions.RemoveEmptyEntries);
+var durations = Array.ConvertAll(times, int.Parse);
+// Assuming that the second line is for distances
+var distances = lines[1].Split(["Distance:", " "], StringSplitOptions.RemoveEmptyEntries);
+var recordDistances = Array.ConvertAll(distances, int.Parse);
+
+for (var i = 0; i < durations.Length; i++) {
+	races.Add(new Race(durations[i], recordDistances[i]));
+	Console.WriteLine(races[i].ToString());
+}
+
+return 0;
+
+record struct Race(int durationInMs, int recordDistanceInMm);
