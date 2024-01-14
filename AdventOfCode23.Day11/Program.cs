5c62c707529025f9ff71325952f99f66c3c8cb42
Markus Bohnert <bohnert.markus@googlemail.com>
2024-01-14 13:26:52 +0100

    Implement expand rows

--- AdventOfCode23.Day11/Program.cs
+++ AdventOfCode23.Day11/Program.cs
@@ -13,50 +13,24 @@ var expanded = ExpandCosmos(lines);
 Console.WriteLine("\nExpanded:");
 PrintGrid(expanded);return 0;
 
-static void PrintGrid(string[] grid) {
+static void PrintGrid(IEnumerable<string> grid) {
 	foreach (var line in grid) {
 		Console.WriteLine(line);
 	}
 }
 
-static string[] ExpandCosmos(string[] input) {
+static List<string> ExpandCosmos(string[] input) {
 	var rows = input.Length;
 	var cols = input[0].Length;
 
-	var expandedRows = rows;
-	var expandedCols = cols;
-
+	var result = input.ToList();
 	for (var i = 0; i < rows; i++) {
-		if (!input[i].Contains('#')) {
-			expandedRows++;
-		}
-	}
-
-	for (var j = 0; j < cols; j++) {
-		if (!input.Any(row => row[j] == '#')) {
-			expandedCols++;
-		}
-	}
-
-	var result = input.Select(row => row.PadRight(expandedCols, '.')).ToList();
-	
-
-	for (int i = 0; i < expandedRows; i++) {
-		if (!input[i].Contains('#')) {
-			result[i+1] = input[i];
+		if (!result[i].Contains('#')) {
+			result.Insert(i, result[i]);
+			rows++;
 			i++;
-		} else {
-			result[i] = input[i];
 		}
 	}
 
-	// for (var j = 0; j < expandedCols; j++) {
-	// 	if (!input.Any(row => row[j] == '#')) {
-	// 		for (var i = 0; i < expandedRows; i += 2) {
-	// 			result[i] = result[i].Insert(j * 2, result[i][j * 2].ToString());
-	// 		}
-	// 	}
-	// }
-
-	return result.ToArray();
+	return result;
 }
