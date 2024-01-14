8b0686c20472e6d03b0c40004083599da4d37ba3
Markus Bohnert <bohnert.markus@googlemail.com>
2024-01-14 14:29:10 +0100

    Refactor Day 11 to separate logic into Cosmos class

---
+++ AdventOfCode23.Day11/Cosmos.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace AdventOfCode23.Day11;
+
+public class Cosmos {
+	readonly IReadOnlyList<string> originalCosmos;
+	readonly List<string> expandedCosmos;
+
+	public Cosmos(IReadOnlyList<string> originalCosmos) {
+		this.originalCosmos = originalCosmos;
+		this.expandedCosmos = this.originalCosmos.ToList();
+	}
+
+	public void PrintOriginalCosmos() {
+    	foreach (var line in this.originalCosmos) {
+    		Console.WriteLine(line);
+    	}
+    }
+
+	public void PrintExpandedCosmos() {
+		foreach (var line in this.expandedCosmos) {
+			Console.WriteLine(line);
+		}
+	}
+    
+	public (int, int) ExpandCosmos() {
+    	var expandedColumns = ExpandColumns(this.expandedCosmos);
+    	var expandedRows = ExpandRows(this.expandedCosmos);
+    
+    	return (expandedColumns, expandedRows);
+    }
+    
+    // Expand columns if they don't contain a #
+    static int ExpandColumns(List<string> grid) {
+    	var sb = new StringBuilder();
+    	var expandedColumns = 0;
+    	// Check if we need to expand the grid
+    	for (var col = 0; col < grid[0].Length; col++) {
+    		// ReSharper disable once AccessToModifiedClosure
+    		var containsUniverse = grid.Any(t => t[col] == '#');
+    
+    		if (!containsUniverse) {
+    			// Expand the grid
+    			for (var index = 0; index < grid.Count; index++) {
+    				sb.Clear();
+    				sb.Append(grid[index]);
+    				sb.Insert(col, '.');
+    
+    				grid[index] = sb.ToString();
+    			}
+    			col++;
+    			expandedColumns++;
+    		}
+    	}
+    
+    	return expandedColumns;
+    }
+    
+    static int ExpandRows(List<string> result) {
+    	var expandedRows = 0;
+    	for (var row = 0; row < result.Count; row++) {
+    		// Expand rows if they don't contain a #
+    		if (!result[row].Contains('#')) {
+    			result.Insert(row, result[row]);
+    			row++;
+    			expandedRows++;
+    		}
+    	}
+    
+    	return expandedRows;
+    }
+}
--- AdventOfCode23.Day11/Program.cs
+++ AdventOfCode23.Day11/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+﻿using AdventOfCode23.Day11;
 using AdventOfCode23.Shared;
 
 var lines = FileHelper.ValidateAndReadInputFile("Input.txt");
@@ -6,72 +6,17 @@ if (lines == null) {
 	return -1;
 }
 
-Console.WriteLine("Original:");
-PrintGrid(lines);
+var cosmos = new Cosmos(lines);
 
-var expanded = ExpandCosmos(lines);
+Console.WriteLine("Original:");
+cosmos.PrintOriginalCosmos();
+
+var expanded = cosmos.ExpandCosmos();
+
+Console.WriteLine($"\nExpanding columns by {expanded.Item1}");
+Console.WriteLine($"Expanding rows by {expanded.Item2}");
 
 Console.WriteLine("\nExpanded:");
-PrintGrid(expanded);return 0;
+cosmos.PrintExpandedCosmos();
 
-static void PrintGrid(IEnumerable<string> grid) {
-	foreach (var line in grid) {
-		Console.WriteLine(line);
-	}
-}
-
-static List<string> ExpandCosmos(string[] input) {
-	var result = input.ToList();
-	
-	// Expand columns if they don't contain a #
-	ExpandColumns(result);
-	ExpandRows(result);
-	
-	return result;
-}
-
-static void ExpandColumns(List<string> grid) {
-	var cols = grid[0].Length;
-	var expandedCols = new HashSet<int>();
-	var sb = new StringBuilder();
-	// Check if we need to expand the grid
-	for (var col = 0; col < cols; col++) {
-		var count = 0;
-		for (var row = 0; row < grid.Count; row++) {
-			if (grid[row][col] == '#') {
-				count++;
-			}
-		}
-		
-		if (count == 0) {
-			// Expand column
-			expandedCols.Add(col);
-		}
-	}
-	
-	if (expandedCols.Count == 0) {
-		return;
-	}
-
-	// Expand the grid
-	for (var index = 0; index < grid.Count; index++) {
-		sb.Clear();
-		sb.Append(grid[index]);
-		var count = 0;
-		foreach (var col in expandedCols) {
-			sb.Insert(col + count, '.');
-			count++;
-		}
-		grid[index] = sb.ToString();
-	}
-}
-
-static void ExpandRows(List<string> result) {
-	for (var row = 0; row < result.Count; row++) {
-		// Expand rows if they don't contain a #
-		if (!result[row].Contains('#')) {
-			result.Insert(row, result[row]);
-			row++;
-		}
-	}
-}
+return 0;
