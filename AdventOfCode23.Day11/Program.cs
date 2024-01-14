e0bf62fa1738d42ab6ade9e2d8afba700d7fdef0
Markus Bohnert <bohnert.markus@googlemail.com>
2024-01-14 14:03:23 +0100

    Implement expand columns

--- AdventOfCode23.Day11/Program.cs
+++ AdventOfCode23.Day11/Program.cs
@@ -1,4 +1,5 @@
-﻿using AdventOfCode23.Shared;
+﻿using System.Text;
+using AdventOfCode23.Shared;
 
 var lines = FileHelper.ValidateAndReadInputFile("Input.txt");
 if (lines == null) {
@@ -20,17 +21,57 @@ static void PrintGrid(IEnumerable<string> grid) {
 }
 
 static List<string> ExpandCosmos(string[] input) {
-	var rows = input.Length;
-	var cols = input[0].Length;
-
 	var result = input.ToList();
-	for (var i = 0; i < rows; i++) {
-		if (!result[i].Contains('#')) {
-			result.Insert(i, result[i]);
-			rows++;
-			i++;
+	
+	// Expand columns if they don't contain a #
+	ExpandColumns(result);
+	ExpandRows(result);
+	
+	return result;
+}
+
+static void ExpandColumns(List<string> grid) {
+	var cols = grid[0].Length;
+	var expandedCols = new HashSet<int>();
+	var sb = new StringBuilder();
+	// Check if we need to expand the grid
+	for (var col = 0; col < cols; col++) {
+		var count = 0;
+		for (var row = 0; row < grid.Count; row++) {
+			if (grid[row][col] == '#') {
+				count++;
+			}
+		}
+		
+		if (count == 0) {
+			// Expand column
+			expandedCols.Add(col);
 		}
 	}
+	
+	if (expandedCols.Count == 0) {
+		return;
+	}
 
-	return result;
+	// Expand the grid
+	for (var index = 0; index < grid.Count; index++) {
+		sb.Clear();
+		sb.Append(grid[index]);
+		var count = 0;
+		foreach (var col in expandedCols) {
+			sb.Insert(col + count, '.');
+			count++;
+		}
+		grid[index] = sb.ToString();
+	}
+}
+
+static void ExpandRows(List<string> result) {
+	for (var row = 0; row < result.Count; row++) {
+		// Expand rows if they don't contain a #
+		if (!result[row].Contains('#')) {
+			result.Insert(row, result[row]);
+			row++;
+		}
+	}
 }
