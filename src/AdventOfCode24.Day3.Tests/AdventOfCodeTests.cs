3b7aeb100039f9b17e0a04154f3fc250cb837d43
Markus Bohnert <bohnert.markus@googlemail.com>
2024-12-07 16:44:49 +0100

    Day 3 Part two WIP

--- src/AdventOfCode24.Day3.Tests/AdventOfCodeTests.cs
+++ src/AdventOfCode24.Day3.Tests/AdventOfCodeTests.cs
@@ -11,4 +11,10 @@ public class AdventOfCodeTests
 	{
 		return _adventOfCodeModule.PartOne([input]);
 	}
+
+	[TestCase("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", ExpectedResult = 48)]
+	public long PartTwo(string input)
+	{
+		return _adventOfCodeModule.PartTwo([input]);
+	}
 }
--- src/AdventOfCode24.Day3/AdventOfCodeModule.cs
+++ src/AdventOfCode24.Day3/AdventOfCodeModule.cs
@@ -23,6 +23,12 @@ public partial class AdventOfCodeModule : IAdventOfCodeModule<long>
 	[GeneratedRegex(@"mul\(([1-9]\d{0,2}),([1-9]\d{0,2})\)", RegexOptions.Compiled)]
 	private static partial Regex MulRegex();
 	
+	[GeneratedRegex(@"do\(\)", RegexOptions.Compiled)]
+	private static partial Regex DoRegex();
+
+	[GeneratedRegex(@"don't\(\)", RegexOptions.Compiled)]
+	private static partial Regex DontRegex();
+	
 	public long PartOne(string[] input)
 	{
 		AnsiConsole.WriteLine("Part One");
@@ -33,8 +39,8 @@ public partial class AdventOfCodeModule : IAdventOfCodeModule<long>
 			var matches = MulRegex().Matches(line);
 			var sum = matches.Sum(match =>
 			{
-				var x = long.Parse(match.Groups[1].Value);
-				var y = long.Parse(match.Groups[2].Value);
+				var x = int.Parse(match.Groups[1].Value);
+				var y = int.Parse(match.Groups[2].Value);
 				return x * y;
 			});
 
@@ -44,6 +50,41 @@ public partial class AdventOfCodeModule : IAdventOfCodeModule<long>
 
 		return totalSum;
 	}
-	
-	public long PartTwo(string[] input) => throw new NotImplementedException();
+
+	public long PartTwo(string[] input)
+	{
+		var totalSum = 0L;
+		for (var i = 0; i < input.Length; i++)
+		{
+			var startIndex = 0;
+			var line = input[i];
+			var currentSum = 0L;
+			var muleEnabled = true;
+			
+			do
+			{
+				currentSum += MulSum(line, ref startIndex, ref muleEnabled);
+			} while (startIndex > 0);
+			
+			totalSum += currentSum;
+		}
+
+		return totalSum;
+	}
+
+	static long MulSum(string line, ref int startIndex, ref bool mulEnabled)
+	{
+		var mulMatch = MulRegex().Match(line, startIndex, line.Length - startIndex);
+		var doMatch = DoRegex().Match(line, startIndex, line.Length - startIndex);
+		var dontMatch = DontRegex().Match(line, startIndex, line.Length - startIndex);
+		
+		startIndex = mulMatch.Index + mulMatch.Length;
+		if (!mulEnabled && doMatch.Index < dontMatch.Index)
+		{
+			return 0;
+		}
+		
+		mulEnabled = (mulMatch.Index < dontMatch.Index) || (doMatch.Index > dontMatch.Index && doMatch.Index > mulMatch.Index);
+		return mulEnabled ? int.Parse(mulMatch.Groups[1].Value) * int.Parse(mulMatch.Groups[2].Value) : 0L;
+	}
 }
