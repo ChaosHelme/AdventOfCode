c6635f5f50eb8f028ace251e2986d2e9a4447294
MarkusB <bohnert.markus@googlemail.com>
2024-12-09 16:16:27 +0100

    Add AdventOfCodeModuleTemplate

---
+++ templates/AdventOfCodeModuleTemplate/AdventOfCodeModule.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCode.Shared;
+
+namespace AdventOfCodeModuleTemplate;
+
+public class AdventOfCodeModule : IAdventOfCodeModule<int>
+{
+	public DateOnly AoCYear => DateOnly.Parse("YYYY-12-DD");
+	public int Day => throw new NotImplementedException();
+	public string Name => "*** Advent Of Code YEAR Day DAY ***";
+
+	public ValueTask RunAsync(string[] input, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+	public int PartOne(string[] input)
+	{
+		return 0;
+	}
+
+	public int PartTwo(string[] input)
+	{
+		return 0;
+	}
+}
